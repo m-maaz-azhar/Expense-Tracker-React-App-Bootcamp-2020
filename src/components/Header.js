be77134ab137af1e2d25964f92840d0fbf548445
m-maaz-azhar <maazazher28@gmail.com>
2020-06-17 00:40:52 +0500

    Initial Commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <div className='header-title'>
+            <h2>
+                EXPENSE TRACKER
+            </h2>
+            <p>(By Muhammad Maaz Azhar)</p>
+        </div>
+    )
+}
